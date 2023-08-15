15ee038d122605f6968f252b20a775d2a0d157e1
bbrfkr <bbrfkr@gmail.com>
2023-08-15 11:45:22 +0000

    fix conf.py

--- .sphinx/conf.py
+++ .sphinx/conf.py
@@ -20,6 +20,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
@@ -30,7 +31,7 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for todo extension ----------------------------------------------
